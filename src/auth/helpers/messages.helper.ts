5d682bedcc2ea555caa7e48f39e1da7e68744319
Matheusash2 <127854649+Matheusash2@users.noreply.github.com>
2023-10-01 13:18:23 -0300

    Criação do API de login

---
+++ src/auth/helpers/messages.helper.ts
@@ -0,0 +1,5 @@
+export const MessagesHelper = {
+  AUTH_LOGIN_NOT_FOUND: 'Login informado inválido',
+  AUTH_PASSWORD_NOT_FOUND: 'Senha informada inválida',
+  AUTH_PASSWORD_OR_LOGIN_NOT_FOUND: 'E-mail e/ou senha não encontrados.',
+};
